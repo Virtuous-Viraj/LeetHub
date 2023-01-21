317b9970c446a1925e2f2af0a7203613d94e7ef7
Viraj Nerlekar <90627072+Virtuous-Viraj@users.noreply.github.com>
2023-01-21 16:15:11 +0530

    Added solution - LeetHub

---
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S[] = read.readLine().split(" ");
+            int R = Integer.parseInt(S[0]);
+            int C = Integer.parseInt(S[1]);
+            int matrix[][] = new int[R][C];
+            int c = 0;
+            for(int i = 0; i < R; i++){
+                String line[]=read.readLine().trim().split(" ");
+                for(int j = 0; j < C; j++){
+                    matrix[i][j] = Integer.parseInt(line[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans = ob.median(matrix, R, C);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    int countLessThanEqual(int[] arr, int mid)
+    {
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high)
+        {
+            int centre = low + (high-low)/2;
+            if(arr[centre]<=mid)
+            {
+                low = centre + 1;
+            }
+            else{
+                high = centre - 1;
+            }
+        }
+        return low;
+    }
+    
+    int median(int matrix[][], int R, int C) {
+        // code here        
+        int r = matrix.length;
+        int c = matrix[0].length;
+        int start = 1;
+        int end = 2000;
+        while(start<=end)
+        {
+            int mid  = start + (end - start)/2;
+            int count = 0;
+            for(int i  = 0; i<r; i++)
+            {
+                count += countLessThanEqual(matrix[i], mid);
+            }
+            if(count <= (r*c)/2)
+            {
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+}
