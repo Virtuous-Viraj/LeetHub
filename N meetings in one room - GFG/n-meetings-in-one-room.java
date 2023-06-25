312ae38c585cfd1a3d407ac5a9ca151e2565ecbb
Viraj Nerlekar <90627072+Virtuous-Viraj@users.noreply.github.com>
2023-06-26 02:29:23 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.java
+++ N meetings in one room - GFG/n-meetings-in-one-room.java
@@ -33,56 +33,51 @@ class GFG {
 // } Driver Code Ends
 
 
-class meeting{
-    int start;
-    int end;
-    int pos;
-    meeting(int start, int end, int pos)
+class myComparator implements Comparator<Meeting>{
+    public int compare(Meeting m, Meeting n)
     {
-        this.start = start;
-        this.end = end;
-        this.pos = pos;
-    }
-}
-class meetingComparator implements Comparator<meeting>
-{
-    public int compare(meeting o1, meeting o2)
-    {
-        if(o1.end < o2.end) return -1;
-        else if(o1.end > o2.end) return 1;
-        else if(o1.pos < o1.pos) return -1;
+        if(m.end < n.end) return -1;
+        else if(m.end > n.end) return 1;
+        else if(m.pos < n.pos) return -1;
         return 1;
     }
 }
-
 class Solution 
 {
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
-    
     public static int maxMeetings(int start[], int end[], int n)
     {
-        // add your code here
-        ArrayList<Integer> ans = new ArrayList<>();
-        ArrayList<meeting> meet = new ArrayList<>();
-        
-        for(int i=0; i<n; i++)
+        ArrayList<Meeting> list = new ArrayList<>();
+        for(int i = 0; i<start.length; i++)
         {
-            meet.add(new meeting(start[i],end[i], i+1));
+            list.add(new Meeting(start[i], end[i], i+1));
         }
-        meetingComparator mc = new meetingComparator();
-        Collections.sort(meet,mc);
-        ans.add(meet.get(0).pos);
-        int currEnd = meet.get(0).end;
+        Collections.sort(list, new myComparator());
+        ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(list.get(0).pos);
+        int endTime = list.get(0).end;
         
-        for(int i = 1; i<start.length; i++)
+        for(int i =1; i<start.length; i++)
         {
-            if(meet.get(i).start > currEnd)
+            if(list.get(i).start > endTime)
             {
-                currEnd = meet.get(i).end;
-                ans.add(meet.get(i).pos);
+                endTime = list.get(i).end;
+                ans.add(list.get(i).start);
             }
         }
+        
         return ans.size();
     }
+}
+class Meeting{
+ int start;
+ int end;
+ int pos;
+ Meeting(int start, int end, int pos)
+ {
+     this.start = start;
+     this.end = end;
+     this.pos = pos;
+ }
 }
