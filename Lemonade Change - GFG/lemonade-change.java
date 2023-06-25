0497e2798d2cb39370980bf2859bcd02764cc35b
Viraj Nerlekar <90627072+Virtuous-Viraj@users.noreply.github.com>
2023-06-26 01:25:19 +0530

    Added solution - LeetHub

---
+++ Lemonade Change - GFG/lemonade-change.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            Solution obj = new Solution();
+            boolean ans = obj.lemonadeChange(n, a);
+            System.out.println(ans ? "True" : "False");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static boolean lemonadeChange(int N, int bills[]) {
+        int fives = 0, tens = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                fives++;
+            } else if (bill == 10) {
+                if (fives == 0) {
+                    return false;
+                }
+                fives--;
+                tens++;
+            } else {
+                if (tens > 0 && fives > 0) {
+                    tens--;
+                    fives--;
+                } else if (fives >= 3) {
+                    fives -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
